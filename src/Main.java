785f42d53383fef714cac56227231e274a9840e1
Zak <zakcng@gmail.com>
2018-03-03 14:30:59 +0000

    Tracking index of currencyPairs Prints index with delimiter : Retrieve currency pair on index

--- src/Main.java
+++ src/Main.java
@@ -19,17 +19,26 @@ public class Main {
         Ticker ticker0 = marketDataService.getTicker(NEO_BTC);
         System.out.println(ticker0.toString());
 
+        //Uses ExchangeData function to retrieve currencyPairs
         List<CurrencyPair> currencyPairs = lib.ExchangeData.getExchangeCurrencyPairs(org.knowm.xchange.coinmarketcap.CoinMarketCapExchange.class.getName());
 
 
+        //Keeps index of current position of currency to be stored.
+        //Will need to be updated if program is saved and reloaded
+        int index = 0;
+        //Converts to string including index
+        //Uses : as delimiter
         for(CurrencyPair currencyPair : currencyPairs){
             if (currencyPair.toString().contains("USD")) {
-                System.out.println(currencyPair);
+                System.out.println(index + (index++) + ":" + currencyPair.toString());
             }
         }
 
-        Ticker ticker1 = marketDataService.getTicker(currencyPairs.get(0));
+        //Retrieve from index
+        Ticker ticker1 = marketDataService.getTicker(currencyPairs.get(380));
         System.out.println(ticker1.toString());
+
+
 
        // System.out.println(currencyPairs.get(0).toString());
 
