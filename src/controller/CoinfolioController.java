dd2302e665a95360596fbaee2a240666e2c05637
Zak <zakcng@gmail.com>
2018-03-09 17:35:41 +0000

    Back to current state pre-MVC ListView fills with currencies

--- src/controller/CoinfolioController.java
+++ src/controller/CoinfolioController.java
@@ -1,13 +1,20 @@
 package controller;
 
+import javafx.collections.FXCollections;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.fxml.Initializable;
 import model.CurrencyList;
 import model.CurrencyPair;
+import org.knowm.xchange.Exchange;
+import org.knowm.xchange.ExchangeFactory;
+import org.knowm.xchange.coinmarketcap.CoinMarketCapExchange;
 import view.ButtonPane;
 import view.CoinfolioRootPane;
 import view.CurrencyListViewPane;
 import view.UserListViewPane;
+import java.util.ArrayList;
+import java.util.List;
 
 public class CoinfolioController {
     private ButtonPane bp;
@@ -16,6 +23,9 @@ public class CoinfolioController {
 
     private CurrencyList model;
     private CoinfolioRootPane view;
+
+    private List<String> cpList = new ArrayList<>();
+
 
     public CoinfolioController(CoinfolioRootPane view, CurrencyList model) {
         this.model = model;
@@ -26,10 +36,11 @@ public class CoinfolioController {
         ulvp = view.getUserListViewPane();
 
         this.attachEventHandlers();
+        this.initialize();
     }
 
     private void attachEventHandlers() {
-    bp.addAddHandler(new AddHandler());
+        bp.addAddHandler(new AddHandler());
     }
 
     public class AddHandler implements EventHandler<ActionEvent> {
@@ -38,7 +49,39 @@ public class CoinfolioController {
             cp.setCurrencyPair("Test");
             clvp.addCurrencyPair(cp);
 
+
         }
+
+    }
+
+    public void initialize() {
+        Exchange exchange = ExchangeFactory.INSTANCE.createExchange(CoinMarketCapExchange.class.getName());
+
+        //Uses ExchangeData function to retrieve currencyPairs
+        List<org.knowm.xchange.currency.CurrencyPair> currencyPairs = lib.ExchangeData.getExchangeCurrencyPairs(org.knowm.xchange.coinmarketcap.CoinMarketCapExchange.class.getName());
+
+
+        //Keeps index of current position of currency to be stored.
+        //Will need to be updated if program is saved and reloaded
+        int index = 0;
+        //Converts to string including index
+        //Uses : as delimiter
+        for (org.knowm.xchange.currency.CurrencyPair currencyPair : currencyPairs) {
+            if (currencyPair.toString().contains("USD")) {
+                CurrencyPair cp = new CurrencyPair();
+                cp.setCurrencyPair(currencyPair.toString());
+                clvp.addCurrencyPair(cp);
+
+            }
+        }
+
+
+        //cvlp.itemsProperty().bind(listProperty);
+        //list.setItems(filteredList);
+
+        //This does not work, you can not directly add to a ListProperty
+        //listProperty.addAll( asianCurrencyList );
+        //listProperty.set(FXCollections.observableArrayList(cpList));
     }
 
 }
